7251cd12b3924f71fb436290d29367701f304f50
Jem <jem91@gmail.com>
2026-02-17 21:57:35 -0500

    Fix JSX error

--- src/App.js
+++ src/App.js
@@ -8068,10 +8068,9 @@ function App() {
   };
 
   return (
-    <>
+    <div className="App smooth-scroll">
       <style>{customStyles}</style>
-      <div className="App smooth-scroll">
-        {view === 'countries' && renderCountrySelection()}
+      {view === 'countries' && renderCountrySelection()}
         {view === 'categories' && renderCategories()}
         {view === 'chambers' && renderChambers()}
         {view === 'parties' && renderParties()}
